f6cf0f12bd658b8fb8ed48d031f43714a90a8906
zh w <2720585098@qq.com>
2022-08-08 08:45:07 +0800

    ✨ feat: 2022/8/8

--- src/main/java/com/xiaowen/config/WebMvcConfig.java
+++ src/main/java/com/xiaowen/config/WebMvcConfig.java
@@ -1,16 +1,29 @@
 package com.xiaowen.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.xiaowen.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.swing.text.Document;
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     // 设置静态资源映射
     @Override
@@ -24,6 +37,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     /**
      * 扩展 mvc 框架的转换器
+     *
      * @param converters
      */
     @Override
@@ -39,5 +53,24 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 
 
 //        super.extendMessageConverters(converters);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xiaowen.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("项目名称")
+                .version("1.0.0")
+                .description("描述")
+                .build();
     }
 }
